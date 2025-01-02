39042f07da4db9bdec63bdfc0a0ae50eb963139a
d3javu1337 <dejja1337@gmail.com>
2025-01-02 12:45:39 +0500

    added nginx

--- src/main/java/org/d3javu/bd/service/ImageService.java
+++ src/main/java/org/d3javu/bd/service/ImageService.java
@@ -6,6 +6,7 @@ import org.d3javu.bd.repositories.ImagesRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,46 +22,70 @@ public class ImageService {
 
     private final ImagesRepository imagesRepository;
 
-    @Value("${app.image.bucket}")
+    @Value("${app.image.bucket.source}")
     private String bucket;
+
+    @Value("${app.image.bucket.local}")
+    private boolean isLocal;
+
+    @Value("${app.image.bucket.port}")
+    private String port;
 
     private final String forPost = bucket+"postimages";
 //    private final String  forAvatar = bucket+"\\profileimages";
 
     @Transactional
     public void uploadAvatar(String path, InputStream content) throws IOException {
-        Path p = Path.of(bucket, "profileimages", path);
+        if (isLocal) {
+            Path p = Path.of(bucket, "profileimages", path);
 
-        try(content){
-            Files.createDirectories(p.getParent());
-            Files.write(p, content.readAllBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            try(content){
+                Files.createDirectories(p.getParent());
+                Files.write(p, content.readAllBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            }
+        }else {
+            final String url = "http://"+this.bucket+":"+this.port+"/profileimages/"+path;
+            new RestTemplate().put(url, content.readAllBytes());
         }
 
     }
 
     @SneakyThrows
     public Optional<byte[]> getAvatar(String path) {
-        Path p = Path.of(bucket, "profileimages", path);
+        if (isLocal) {
+            Path p = Path.of(bucket, "profileimages", path);
 
-        return Files.exists(p) ? Optional.of(Files.readAllBytes(p)) : Optional.empty();
+            return Files.exists(p) ? Optional.of(Files.readAllBytes(p)) : Optional.empty();
+        }
+        final String url = "http://"+this.bucket+":"+this.port+"/profileimages/"+path;
+        return Optional.ofNullable(new RestTemplate().getForObject(url, byte[].class));
     }
 
     @Transactional
     public void uploadImage(String path, InputStream content) throws IOException {
-        Path p = Path.of(bucket, "postimages", path);
+        if (isLocal) {
+            Path p = Path.of(bucket, "postimages", path);
 //        System.out.println("--------------------------------"+true);
-        try(content){
-            Files.createDirectories(p.getParent());
-            Files.write(p, content.readAllBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            try(content){
+                Files.createDirectories(p.getParent());
+                Files.write(p, content.readAllBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            }
+        }else {
+            final String url = "http://"+this.bucket+":"+this.port+"/postimages/"+path;
+            new RestTemplate().put(url, content.readAllBytes());
         }
 
     }
 
     @SneakyThrows
     public Optional<byte[]> getImage(String path) {
-        Path p = Path.of(bucket, "postimages", path);
+        if (isLocal) {
+            Path p = Path.of(bucket, "postimages", path);
 
-        return Files.exists(p) ? Optional.of(Files.readAllBytes(p)) : Optional.empty();
+            return Files.exists(p) ? Optional.of(Files.readAllBytes(p)) : Optional.empty();
+        }
+        final String url = "http://"+this.bucket+":"+this.port+"/postimages/"+path;
+        return Optional.ofNullable(new RestTemplate().getForObject(url, byte[].class));
     }
 
     @Transactional
